2d499596823108d58cd6d02d9087ba308f60a412
Aman Dugar <amanamandugar@gmail.com>
2023-04-30 18:08:58 +0530

    Init Client

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+export const corePlugins = {
+  preflight: false,
+};
